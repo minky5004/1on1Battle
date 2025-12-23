f7f41059dcf23fd8f0842519e7fa788c3157f825
minky5004 <minky5004@gmail.com>
2025-12-23 20:00:36 +0900

    챔피언 추상 메소드

--- Champion/Champion.java
+++ Champion/Champion.java
@@ -17,6 +17,43 @@ public abstract class Champion {
         this.defense = defense;
     }
 
-    public void basic
+    // 기본공격
+    public void basicAttack(Champion champion, Champion target){
+        System.out.println(this.name + "의 기본 공격");
+        target.takeDamage(champion, this.attackDamage);
+    }
+
+    // 데미지 받기
+    public void takeDamage(Champion champion, int damage){
+
+        int realDamage = damage - defense;
+
+        if (realDamage <= 0){
+            realDamage = 0;
+        }
+
+        hp = hp - realDamage;
+
+        if (hp > 0){
+            System.out.println(name + "이" + realDamage + "의 피해를 받음. (남은 체력 : " + hp + ")");
+        } else if (hp < 0) {
+            System.out.println(name + "이 죽음.");
+        }
+    }
+
+    public void levelUp(){
+        System.out.println(name + "이 레벨업!");
+        this.level++;
+        hp += 100;
+        attackDamage += 50;
+        defense += 50;
+    }
+
+
+    public abstract void leftHook(Champion champion, Champion target);
+    public abstract void rightHook(Champion champion, Champion target);
+    public abstract void leftKick(Champion champion, Champion target);
+    public abstract void rightKick(Champion champion, Champion target);
+    public abstract void combo(Champion champion, Champion target);
 
 }
