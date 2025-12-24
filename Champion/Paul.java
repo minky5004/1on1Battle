e57c0a74b738ef9e4b2856f061daedea8b4ee3d9
minky5004 <minky5004@gmail.com>
2025-12-24 10:13:22 +0900

    Paul 개성 추가

--- Champion/Paul.java
+++ Champion/Paul.java
@@ -2,44 +2,57 @@ package Champion;
 
 public class Paul extends Champion {
 
-    public Paul (String name, int level, int hp, int attackDamage, int defense) {
-        super(name, level, hp, attackDamage,defense);
+    public Paul(String name, int level, int hp, int attackDamage, int defense) {
+        super(name, level, hp, attackDamage, defense);
     }
+
+    private int charge = 0;
 
     @Override
     public void leftHand(Champion champion, Champion target) {
-        System.out.println("Paul의 LeftHand");
-        target.takeDamage(champion.getAttackDamage()-20);
+        System.out.println("Paul의 잽");
+        target.takeDamage(champion.getAttackDamage() - 20);
     }
 
     @Override
     public void rightHand(Champion champion, Champion target) {
-        System.out.println("Paul의 RightHand!");
-        target.takeDamage(champion.getAttackDamage()-10);
+        if (charge >= 3) {
+            System.out.println("Paul의 붕권!");
+            target.takeDamage(champion.getAttackDamage() * 2);
+        } else {
+            System.out.println("Paul의 정권!");
+            target.takeDamage(champion.getAttackDamage() - 10);
+        }
     }
 
     @Override
     public void leftKick(Champion champion, Champion target) {
-        System.out.println("Paul의 LeftKick!");
+        System.out.println("Paul의 로우킥");
+        target.takeDamage(20);
     }
 
     @Override
     public void rightKick(Champion champion, Champion target) {
-        System.out.println("Paul의 rightKick!.");
+        System.out.println("Paul의 기모으기!.");
+        this.charge++;
+        System.out.println("기모으기 " + charge + " 단계");
     }
 
     @Override
     public void combo(Champion champion, Champion target) {
-        System.out.println("Steve의 콤보");
-        target.takeDamage(champion.getAttackDamage()*2);
+        System.out.println("Paul의 콤보");
+        target.takeDamage(champion.getAttackDamage());
+        target.takeDamage(champion.getAttackDamage());
 
     }
 
     @Override
     public void specialMove(Champion champion, Champion target) {
-        System.out.println("Paul의 붕권");
-        target.takeDamage(champion.getAttackDamage()*2);
-
+        if (charge >= 3) {
+            System.out.println("Paul의 빅뱅 붕권!");
+            target.takeDamage(champion.getAttackDamage() * 3);
+        } else {
+            System.out.println("special Move 실패!");
+        }
     }
-
 }
